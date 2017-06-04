a5c2ebe48a2cd6a599e03ae589b3d6c62976d829
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-04 23:46:28 +0900

    change code

--- gcd01.sh
+++ gcd01.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
+read -p "please input num1 and numb2:" a b
 
-function gcd()
- {
-   num1=$1
-   num2=$2
-   r=1
-   until [ "$r" -eq 0 ]
-   do
-     let "r = $num1 % $num2"
-     num1=$num2
-     num2=$r
-   done
- }
- gcd 24 86
- echo " 24 and 86 の最大公約数 = $num1"
+num1=$a
+num2=$b
+r=1
+until [ "$r" -eq 0 ]
+do
+ r=$(($num1%$num2))
+ num1=$num2
+ num2=$r
+done
+echo " $a and $b の最大公約数 = $num1"
